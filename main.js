bbd8a7c0f39ecdc6e80aa9102c3e0ccfb21effc0
EmmaLanka <103385254+EmmaLanka@users.noreply.github.com>
2022-05-15 22:06:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery'
+global.jQuery = $
+global.$ = $
+
+require('slow-show.js')
+require('focus-visible.js')
+require('keyboard-click.js')
---
+++ new.js
@@ -0,0 +1,45 @@
+function closeMenu(el) {
+  let toggler = document.getElementById('menu-toggle');
+  console.log(toggler);
+  toggler.click()
+  smoothScroll(el, 150)
+}
+function scrollToElem(el) {
+  smoothScroll(el, 150)
+}
+function getTop (element, start) {
+  if (element) {
+    return element.getBoundingClientRect().top + start
+  } else {
+    return start
+  }
+}
+function smoothScroll (scrollTo, offset) {
+
+  let requestAnimationFrame;
+
+  const easeInOutCubic = t => t < 0.5 ? 16 * t * t * t * t * t  : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+
+  if (!requestAnimationFrame) {
+    requestAnimationFrame = window.requestAnimationFrame || (fn => window.setTimeout(fn, 16))
+  }
+
+  const startPoint = window.scrollTop || window.pageYOffset;
+  const end = this.getTop(scrollTo, startPoint) - offset;
+
+  const clock = Date.now();
+  const step = () => {
+    // The time elapsed from the beginning of the scroll
+    const elapsed = Date.now() - clock;
+    // Calculate the scroll position we should be in
+    const scrolling = elapsed < 1000;
+    const position = scrolling ? startPoint + (end - startPoint) * easeInOutCubic(elapsed / 1000) : end;
+
+    if (scrolling) {
+      requestAnimationFrame(step);
+    }
+
+    window.scrollTo(0, position);
+  }
+  step()
+}
